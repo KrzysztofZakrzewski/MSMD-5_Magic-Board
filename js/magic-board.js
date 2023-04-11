4378c92538b54590555a099025564db3a4663e42
Krzysztof Zakrzewski <krzysztof.zakrzewski@protonmail.com>
2023-04-11 22:35:25 +0200

    basic js components

---
+++ js/magic-board.js
@@ -0,0 +1,37 @@
+const container = document.querySelector('container');
+
+let spanBg = '#1e1f26',
+	shape = 'square',
+	mode = 'draw',
+	color = 'ff0000',
+	singeleColor = false,
+	colors = [
+        '#ff0000',
+        '#ff4000',
+        '#ff8000',
+        '#ffbf00',
+        '#ffff00',
+        '#bfff00',
+        '#80ff00',
+        '#40ff00',
+        '#00ff00',
+        '#00ff40',
+        '#00ff80',
+        '#00ffbf',
+        '#00ffff',
+        '#00bfff',
+        '#0080ff',
+        '#0040ff',
+        '#0000ff',
+        '#4000ff',
+        '#8000ff',
+        '#bf00ff',
+        '#ff00ff',
+        '#ff00bf',
+        '#ff0080',
+        '#ff0040',
+        '#ff0000',
+    ];
+
+
+    
